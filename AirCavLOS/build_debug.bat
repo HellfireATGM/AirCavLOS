0fab473a2e182a599343a11c2eae57c012ddcc4b
Brian McDonald <96739000+HellfireATGM@users.noreply.github.com>
2022-01-19 08:23:23 -0800

    add build scripts

---
+++ AirCavLOS/build_debug.bat
@@ -0,0 +1,12 @@
+@set root=%cd%
+
+@echo Setting up Visual Studio 2015 environment
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32"
+
+cd %root%
+devenv AirCavLOS.sln /rebuild "Debug|Win32" 
+devenv AirCavLOS.sln /rebuild "Debug|x64" 
+
+:end
+cd %root%
+@pause
